21c5e312580444e490ee11065d9fba4cbc46d1b7
sborkar009 <141212389+sborkar009@users.noreply.github.com>
2023-08-29 18:53:23 +0530

    Add files via upload

---
+++ PARKINGTICKET NEW.sql
@@ -0,0 +1,107 @@
+create database parkingticket
+use parkingticket
+
+CREATE TABLE Vehicle ( VehicleID INT IDENTITY(200, 1), VehicleNoPlate VARCHAR(50), OwnerName VARCHAR(50),ContactNo VARCHAR(10), CreatedOn DATETIME DEFAULT GETDATE());
+CREATE TABLE ParkingSpace ( SpaceID INT IDENTITY(400, 1),lotname varchar(50),NOOFSPACE INT,CreatedOn DATETIME DEFAULT GETDATE());
+CREATE TABLE ParkingTicket (TicketID INT IDENTITY(300, 1),VehicleID int,SpaceID INT,EntryDateTime DATETIME, ExitDateTime DATETIME, CreatedOn DATETIME DEFAULT GETDATE());
+create table Totalamount (amountid int identity(500,1),VehicleID int,TicketID int,vehiclename varchar(50),amount int,parkingduration float,)
+
+exec Vehicle1 0,'MH34 GD 0009','SHUBHAM','8668582779','I'
+exec Vehicle1 0,'MH01 FD 0007','AKSHAY','9868582779','I'
+exec Vehicle1 0,'MH12 SD 8855','RAHUL','6668582779','I'
+exec Vehicle1 0,'GJ YD 9999','GUNJAN','7668582779','I'
+exec Vehicle1 0,'GJ QD 0001','ROHIT','8468582779','I'
+exec Vehicle1 0,'MH34 JH 5555','DIPAK','7068582779','I'
+
+
+exec ParkingSpace1 0,'Lot-A',100,'I'
+exec ParkingSpace1 0,'Lot-B',50,'I'
+
+
+exec ParkingTicket12 0,200,400,'2023-08-21','2023-08-21','I'
+exec ParkingTicket12 0,201,400,'2023-08-21','2023-08-21','I'
+exec ParkingTicket12 0,202,401,'2023-08-20','2023-08-20','I'
+exec ParkingTicket12 0,203,401,'2023-08-22','2023-08-22','I'
+exec ParkingTicket12 0,204,400,'2023-08-21','2023-08-21','I'
+exec ParkingTicket12 0,205,400,'2023-08-25','2023-08-26','I'
+
+
+
+
+exec Totalamount1 0,200,300,'THAR',50,3.0,'I'
+exec Totalamount1 0,201,301,'HARRIER',50,4.0,'I'
+exec Totalamount1 0,201,302,'FORTUNER',50,6.0,'I'
+exec Totalamount1 0,202,303,'BMW',50,2.0,'I'
+exec Totalamount1 0,203,304,'BENTLY',50,8.0,'I'
+exec Totalamount1 0,204,305,'AUDI',50,5.0,'I'
+exec Totalamount1 0,205,306,'MINI COPPER',50,1.0,'I'
+
+
+
+delete from ParkingTicket where TicketID=305
+
+
+
+select *from Vehicle
+select *from ParkingSpace
+select *from ParkingTicket
+select *from Totalamount
+
+
+
+  alter TRIGGER updateAmountOnTotal
+ON Totalamount
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @newAmountId INT, @newParkingDuration FLOAT;
+
+    SELECT @newAmountId = amountid, @newParkingDuration = parkingduration FROM inserted;
+
+    DECLARE @newAmount DECIMAL(6, 2);
+    SET @newAmount = @newParkingDuration;
+
+    UPDATE Totalamount
+    SET amount = amount * @newAmount
+    WHERE amountid = @newAmountId;
+END;
+
+
+
+
+
+CREATE TRIGGER UpdateParkingSpaceOnEntry
+ON ParkingTicket
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @SpaceID INT, @EntryDateTime DATETIME;
+
+    SELECT @SpaceID = SpaceID, @EntryDateTime = EntryDateTime
+    FROM inserted;
+
+    UPDATE ParkingSpace
+    SET NOOFSPACE = NOOFSPACE - 1
+    WHERE SpaceID = @SpaceID;
+END;
+
+
+
+
+CREATE TRIGGER UpdateParkingSpaceOnExit2
+ON ParkingTicket
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @SpaceID INT;
+
+    SELECT @SpaceID = d.SpaceID
+    FROM deleted d;
+
+    IF @SpaceID IS NOT NULL
+    BEGIN
+        UPDATE ParkingSpace
+        SET NOOFSPACE = NOOFSPACE + 1
+        WHERE SpaceID = @SpaceID;
+    END;
+END;
