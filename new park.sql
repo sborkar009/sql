1a8b3fa8e6c359f8f3bebb05a99ef91f8136d10f
sborkar009 <141212389+sborkar009@users.noreply.github.com>
2023-09-01 18:41:58 +0530

    Add files via upload

---
+++ new park.sql
@@ -0,0 +1,116 @@
+create database parkdb
+use parkdb
+
+create table vehicletype(
+vehicletypeid int identity(100,1),
+typeofvehicle varchar(50),
+noofspace int,
+rateperhour decimal(10,2))
+
+CREATE TABLE Vehicle ( 
+VehicleID INT IDENTITY(200, 1),
+vehicletypeid int,
+VehicleNoPlate VARCHAR(50), 
+vehiclename varchar(50),
+OwnerName VARCHAR(50),
+ContactNo VARCHAR(10), 
+CreatedOn DATETIME DEFAULT GETDATE())
+
+
+CREATE TABLE ParkingTicket (
+TicketID INT IDENTITY(300, 1),
+VehicleID int,
+EntryDate  date, 
+ExitDate date,
+TicketAmount decimal (10,2),
+parkingduration decimal(10,2),
+CreatedOn DATETIME DEFAULT GETDATE())
+
+create table adminlogin(
+adminloginid int identity(400,1),
+username varchar(50),
+password varchar(50)
+)
+
+
+
+drop table ParkingTicket
+
+exec vehicletype1 0,'Four wheeler',50,30,'I'
+exec vehicletype1 0,'Two wheeler',50,15,'I'
+
+
+
+exec Vehicle1 0,100,'MH34 HG 0007','SWIFT','SHUBHAM','8668582779','I'
+exec Vehicle1 0,101,'MH34 HG 0008','SPLENDER','SHUBHAM','8668582779','I'
+
+
+EXEC ParkingTicket1 0,200,'2023-08-30','2023-08-30',50,4,'I'
+EXEC ParkingTicket1 0,201,'2023-08-30','2023-08-30',50,4,'I'
+
+
+
+exec adminlogin1 0,'sborkar009@gmail.com','shubham@1','I'
+
+delete from Vehicle where VehicleID=203
+
+
+select * from vehicletype
+select* from Vehicle
+select *from ParkingTicket
+select * from adminlogin
+
+
+
+create TRIGGER totalAmount
+ON ParkingTicket 
+AFTER INSERT
+AS 
+BEGIN
+    -- Update ticketamount for the newly inserted records
+    UPDATE pt
+    SET pt.TicketAmount = pt.parkingduration * vt.rateperhour
+    FROM ParkingTicket pt
+    INNER JOIN inserted i ON pt.TicketID = i.TicketID
+    INNER JOIN vehicle v ON pt.VehicleID = v.VehicleID
+    INNER JOIN vehicletype vt ON v.vehicletypeid = vt.vehicletypeid;
+END;
+
+
+
+CREATE TRIGGER VehicleEntry
+ON Vehicle
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @VehicleTypeID INT
+
+ 
+     -- Get the VehicleTypeID of the newly inserted vehicle
+    SELECT @VehicleTypeID = vehicletypeid
+    FROM inserted
+  -- Update the noofspace column in vehicletype
+    UPDATE vehicletype
+    SET noofspace = noofspace - 1
+    WHERE vehicletypeid = @VehicleTypeID
+END;
+
+
+
+
+CREATE TRIGGER VehicleExit
+ON Vehicle
+AFTER DELETE
+AS
+BEGIN
+    DECLARE @VehicleTypeID INT
+
+    -- Get the VehicleTypeID of the deleted vehicle
+    SELECT @VehicleTypeID = vehicletypeid
+    FROM deleted
+
+    -- Update the noofspace column in vehicletype
+    UPDATE vehicletype
+    SET noofspace = noofspace + 1
+    WHERE vehicletypeid = @VehicleTypeID
+END;
---
+++ new park2.sql
@@ -0,0 +1,162 @@
+use parkdb
+
+
+-- Template generated from Template Explorer using:
+
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+
+
+		   
+create PROCEDURE vehicletype1
+@vehicletypeid int,
+@typeofvehicle varchar(50),
+@noofspace int,
+@rateperhour decimal(10,2),
+@Type varchar(50)
+
+as begin
+
+	SET NOCOUNT ON;
+
+	if (@vehicletypeid=0 and @Type='I')
+	begin
+	insert into vehicletype values (@typeofvehicle,@noofspace,@rateperhour)
+	end
+	else
+	begin
+	if (@Type='U')
+	begin
+	update vehicletype set typeofvehicle=@typeofvehicle,noofspace=@noofspace,rateperhour=@rateperhour where vehicletypeid=@vehicletypeid
+	end
+	else
+	begin
+	if (@Type='D')
+	delete from vehicletype  where  vehicletypeid=@vehicletypeid
+	end
+	end
+END
+GO
+
+
+
+
+CREATE PROCEDURE Vehicle1 (
+    @VehicleID INT,
+    @vehicletypeid INT,
+    @VehicleNoPlate VARCHAR(50),
+    @vehiclename VARCHAR(50),
+    @OwnerName VARCHAR(50),
+    @ContactNo VARCHAR(10),
+    @Type VARCHAR(1) 
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF (@VehicleID = 0 AND @Type = 'I')
+    BEGIN
+        INSERT INTO Vehicle VALUES (@vehicletypeid, @VehicleNoPlate, @vehiclename, @OwnerName, @ContactNo, GETDATE())
+    END
+    ELSE
+    BEGIN
+        IF (@Type = 'U')
+        BEGIN
+            UPDATE Vehicle SET vehicletypeid = @vehicletypeid, VehicleNoPlate = @VehicleNoPlate, vehiclename = @vehiclename, OwnerName = @OwnerName, ContactNo = @ContactNo WHERE VehicleID = @VehicleID
+        END
+        ELSE
+        BEGIN
+            IF (@Type = 'D')
+            BEGIN
+                DELETE FROM Vehicle WHERE VehicleID = @VehicleID
+            END
+           END
+           END
+           END
+
+
+
+
+
+
+		   
+create PROCEDURE ParkingTicket1
+@TicketID INT ,
+@VehicleID int,
+@EntryDate DATE,
+@ExitDate DATE, 
+@TicketAmount decimal (10,2),
+@parkingduration decimal(10,2),
+@Type varchar(50)
+
+as begin
+
+	SET NOCOUNT ON;
+
+	if (@TicketID=0 and @Type='I')
+	begin
+	insert into ParkingTicket values (@VehicleID,@EntryDate,@ExitDate,@TicketAmount,@parkingduration,GETDATE())
+	end
+	else
+	begin
+	if (@Type='U')
+	begin
+	update ParkingTicket set VehicleID=@VehicleID,EntryDate=@EntryDate,ExitDate=@ExitDate,TicketAmount=@TicketAmount,parkingduration=@parkingduration where TicketID=@TicketID
+	end
+	else
+	begin
+	if (@Type='D')
+	delete from ParkingTicket where TicketID=@TicketID
+	end
+	end
+END
+GO
+
+
+alter PROCEDURE adminlogin1
+@adminloginid int,
+@username varchar(50),
+@password varchar(50),
+@Type varchar(50)
+
+as begin
+
+	SET NOCOUNT ON;
+
+	if (@adminloginid=0 and @Type='I')
+	begin
+	insert into adminlogin values (@username,@password)
+	end
+	else
+	begin
+	if (@Type='U')
+	begin
+	update adminlogin set username=@username,password=@password where adminloginid=@adminloginid
+	end
+	else
+	begin
+	if (@Type='D')
+	delete from adminlogin where adminloginid=@adminloginid
+	end
+	end
+END
+GO
+
+
+
